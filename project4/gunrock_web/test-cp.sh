4b0fb3b90c27c9bdb759ecf941f16e0e04481d89
Travis L <liangtravis910@gmail.com>
2024-12-04 20:23:08 +0000

    trying individual test cases for cp and rm to identify issues with functionality

--- project4/gunrock_web/test-cp.sh
+++ project4/gunrock_web/test-cp.sh
@@ -1,61 +1,67 @@
 #!/bin/bash
-set -e
 
+echo "Running ds3cp tests..."
+
+
+# # Test 1: Copy a small file
+# echo "Test 1: Copy a small file"
+# cp tests/disk_images/a.img a2.img
+# ./ds3cp a2.img tests/A.txt 3
+# ./ds3bits a2.img
+# ./ds3ls a2.img /a/b
+# echo -e "\n"
+
+# Test 2: Copy a large file
+echo "Test 2: Copy a large file"
 cp tests/disk_images/a.img a2.img
-
-# Check folder structure
-echo "Checking root directory"
-./ds3ls a2.img /
-echo "Making files"
-
-# Making files
-./ds3touch a2.img 0 F.txt
-./ds3ls a2.img /
-
-# Test order of copying files
-# 1. Copy A to F
-# 2. Copy A to F again
-# 3. Copy B to F
-# 4. Copy C to F
-# 5. Copy B to F
-# 6. Copy A to F
-# Copy D to F
-# Note: can also test file taking up between one and two blocks of space/do other test cases
-
-echo "First Test: Copy small file to F.txt"
-./ds3cp a2.img tests/A.txt 4
-./ds3cat a2.img 4
+echo "Before copying ds3bits:"
+./ds3bits a2.img
+./ds3cp a2.img tests/C.txt 3
+echo -e "\n"
+echo "After copying ds3bits:"
+./ds3bits a2.img
+./ds3ls a2.img /a/b
 echo -e "\n"
 
+# # Test 3: Overwriting an existing file
+# echo "Test 3: Overwriting an existing file"
+# cp tests/disk_images/a.img a2.img 
+# ./ds3cp a2.img tests/A.txt 3
+# ./ds3cp a2.img tests/B.txt 3
+# ./ds3bits a2.img
+# ./ds3ls a2.img /a/b
+# echo -e "\n"
 
-echo "Second Test: Copy small file to F.txt again"
-./ds3cp a2.img tests/A.txt 4
-./ds3cat a2.img 4
-echo -e "\n"
+# # Test 4: Copy a file to a non-empty directory
+# cp tests/disk_images/a.img a2.img 
+# ./ds3mkdir a2.img 0 newdir
+# ./ds3cp a2.img tests/A.txt 4
+# ./ds3bits a2.img
+# ./ds3ls a2.img /a/b
+# echo -e "\n"
 
-echo "Third Test: Copy file that takes one block of space"
-./ds3cp a2.img tests/B.txt 4
-./ds3cat a2.img 4
-echo -e "\n"
+# # Test 5: Insufficient space in data region
+# echo "Test 5: Insufficient space in data region"
+# cp tests/disk_images/a.img a2.img
+# for i in {1..28}; do
+#     ./ds3touch a2.img 0 testfile$i
+# done
+# ./ds3cp a2.img tests/A.txt 3
+# ./ds3bits a2.img
+# ./ds3ls a2.img /a/b
+# echo -e "\n"
 
-echo "Fourth Test: Copy file that takes two block of space"
-./ds3cp a2.img tests/C.txt 4
-./ds3cat a2.img 4
-echo -e "\n"
+# # Test 6: Invalid Source file
+# echo "Test 6: Invalid Source file"
+# cp tests/disk_images/a.img a2.img
+# ./ds3cp a2.img tests/invalid.txt 3
+# ./ds3bits a2.img
+# ./ds3ls a2.img /a/b
+# echo -e "\n"
 
-echo "Fifth Test: Copy file that takes one block of space again"
-./ds3cp a2.img tests/B.txt 4
-./ds3cat a2.img 4
-echo -e "\n"
-
-echo "Sixth Test: Copy file that takes one small amout of space again"
-./ds3cp a2.img tests/A.txt 4
-./ds3cat a2.img 4
-echo -e "\n"
-
-echo "Seventh Test: Copy empty file"
-./ds3cp a2.img tests/D.txt 4
-./ds3cat a2.img 4
-echo -e "\n"
-
-echo "Test complete"
+# # Test 7: Invalid Destination file
+# echo "Test 7: Invalid Destination file"
+# cp tests/disk_images/a.img a2.img
+# ./ds3cp a2.img tests/A.txt 9999
+# ./ds3bits a2.img
+# ./ds3ls a2.img /a/b
--- project4/gunrock_web/test-rm.sh
+++ project4/gunrock_web/test-rm.sh
@@ -1,27 +1,49 @@
 #!/bin/bash
+echo "Running rm tests"
 cp tests/disk_images/a.img a2.img
+./ds3bits a2.img
+
+# # Test: removing a file already inside the disk
+# echo "Test: removing a file already inside the disk"
+# ./ds3ls a2.img /a/b
+# ./ds3rm a2.img 2 c.txt
+# echo "Directory listing after removal:"
+# ./ds3ls a2.img /a/b
+# ./ds3bits a2.img
+# echo -e "\n"
+# echo "Now removing a directory nested in b"
+# ./ds3rm a2.img 1 b
+# ./ds3ls a2.img /a
+# ./ds3bits a2.img
 
 # First test
 echo "First test: removing an empty directory"
 ./ds3mkdir a2.img 0 testdir1
 echo "Directory listing before removal:"
 ./ds3ls a2.img /
+
+./ds3bits a2.img
 echo "Attempting to remove 'testdir1'"
 ./ds3rm a2.img 0 testdir1
 echo "Directory listing after removal:"
 ./ds3ls a2.img /
 echo -e "\n"
+./ds3bits a2.img
 
-# Second test
+
+Second test
 echo "Second test: removing a empty file"
 cp tests/disk_images/a.img a2.img
 ./ds3touch a2.img 0 testfile1
 echo "Directory listing before removal:"
 ./ds3ls a2.img /
+./ds3bits a2.img
+
 echo "Attempting to remove 'testfile1'"
 ./ds3rm a2.img 0 testfile1
 echo "Directory listing after removal:"
 ./ds3ls a2.img /
+./ds3bits a2.img
 echo -e "\n"
 
 # Third test
@@ -34,7 +56,6 @@ echo "Directory size:"
 
 # Add files
 ./ds3touch a2.img 0 testfile2
-./ds3cp a2.img tests/B.txt 4
 ./ds3cat a2.img 4
 
 # Directory size after adding files
@@ -58,8 +79,12 @@ cp tests/disk_images/a.img a2.img
 ./ds3touch a2.img 0 testfile5
 echo "Directory listing before removal:"
 ./ds3ls a2.img /
+./ds3bits a2.img
+
+
 echo "Attempting to remove 'testfile4'"
 ./ds3rm a2.img 0 testfile4
 echo "Directory listing after removal:"
 ./ds3ls a2.img /
+./ds3bits a2.img
 echo -e "\n"
